e42a0a8cdc9225b46c6edb2648d18b8f073e8881
Alexander Rosa <alexanderrosa@alexanders-mbp.lan>
2017-07-21 20:48:14 -0600

    func: added Route components for contact list and create contact

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 import ListContacts from './ListContacts';
 import CreateContact from './CreateContact'
 import * as ContactsAPI from './utils/ContactsAPI';
 
 class App extends Component {
   state = {
-    screen: 'list',  // list; create
     contacts: []
   };
   componentDidMount() {
@@ -19,22 +19,16 @@ class App extends Component {
     }));
     ContactsAPI.remove(contact);
   }
-  navigateToCreate = () => {
-    this.setState({ screen: 'create' });
-  }
   render() {
     return (
-      <div>
-        {this.state.screen === 'list' && (
+      <div className='app'>
+        <Route exact path='/' render={() => (
           <ListContacts
-            contacts={this.state.contacts}
-            onDeleteContact={this.removeContact}
-            onNavigate={this.navigateToCreate}
+          contacts={this.state.contacts}
+          onDeleteContact={this.removeContact}
           />
-        )};
-        {this.state.screen === 'create' && (
-          <CreateContact />
-        )}
+        )}/>
+        <Route path='/create' component={CreateContact} />
       </div>
     )
   }
